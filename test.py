e88d08fec17c90a70970f5c59ab447673b7eaea1
00-hidan-00 <david.taranov@gmail.com>
2022-07-11 22:55:27 +0200

    edit astronauts_with_aiohttp

--- test.py
+++ test.py
@@ -1,28 +1,21 @@
 import csv
 import io
+from app import CHARACTERISTICS_URL
+import urllib.request
 
-import aiofiles
-import aiohttp
-import asyncio
-import requests
-import json
-
-from aiohttp import web
-from flask import ctx
-from requests import auth
-
-height_list = []
-weight_list = []
-CHARACTERISTICS_API = 'https://drive.google.com/uc?export=download&id=1yM0a4CSf0iuAGOGEljdb7qcWyz82RBxl'
-
-response = requests.get(CHARACTERISTICS_API)
-csv_data = io.StringIO(response.text)
-reader = csv.DictReader(csv_data, delimiter=',')
-for row in reader:
-    height_list.append(float(row[' "Height(Inches)"']))
-    weight_list.append(round(float(row[' "Weight(Pounds)"']), 1))
-average_height = round(((max(height_list) + min(height_list)) / 2) * 2.54, 2)
-average_weight = round(((max(weight_list) + min(weight_list)) / 2) / 2.2046, 2)
-
-print(average_height)
-print(average_weight)
+def characteristics():
+    height_list = []
+    weight_list = []
+    response = urllib.request.urlopen("https://drive.google.com/uc?export=download&id=1yM0a4CSf0iuAGOGEljdb7qcWyz82RBxl")
+    # response = urllib.request.urlopen(CHARACTERISTICS_URL)
+    with io.TextIOWrapper(response, encoding='utf-8') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            print(row)
+#             height_list.append(float(row[' "Height(Inches)"']))
+#             weight_list.append(float(row[' "Weight(Pounds)"']))
+#         average_height = round(((max(height_list) + min(height_list)) / 2) * 2.54, 2)
+#         average_weight = round(((max(weight_list) + min(weight_list)) / 2) / 2.2046, 2)
+#         return f"""<p>Средний вес: {average_weight} кг</p>
+#         <p>Средний рост: {average_height} см</p>
+# """
