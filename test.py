7269050b50eedbb5bea7cab802809e5a4fced398
00-hidan-00 <david.taranov@gmail.com>
2022-07-12 10:27:23 +0200

    edit app

--- test.py
+++ test.py
@@ -1,21 +1,42 @@
-import csv
-import io
-from app import CHARACTERISTICS_URL
-import urllib.request
+import asyncio
+import json
 
-def characteristics():
-    height_list = []
-    weight_list = []
-    response = urllib.request.urlopen("https://drive.google.com/uc?export=download&id=1yM0a4CSf0iuAGOGEljdb7qcWyz82RBxl")
-    # response = urllib.request.urlopen(CHARACTERISTICS_URL)
-    with io.TextIOWrapper(response, encoding='utf-8') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            print(row)
-#             height_list.append(float(row[' "Height(Inches)"']))
-#             weight_list.append(float(row[' "Weight(Pounds)"']))
-#         average_height = round(((max(height_list) + min(height_list)) / 2) * 2.54, 2)
-#         average_weight = round(((max(weight_list) + min(weight_list)) / 2) / 2.2046, 2)
-#         return f"""<p>Средний вес: {average_weight} кг</p>
-#         <p>Средний рост: {average_height} см</p>
-# """
+import aiohttp
+from aiohttp import web
+from flask import Flask
+
+from app import SPACE_API
+
+
+# app = Flask(__name__)
+
+
+
+
+
+# @app.route('/astronauts-with-aiohttp')
+# async def astronauts_with_aiohttp():
+#     async with aiohttp.ClientSession() as session:
+#         async with session.get(SPACE_API) as response:
+#             return f'Количество космонавтов в данный момент:'
+
+
+# app = web.Application()
+# app.add_routes([web.get('/', astronauts_with_aiohttp),
+#                 web.get('/{name}', astronauts_with_aiohttp)])
+
+
+
+# if __name__ == '__main__':
+#     web.run_app(app)
+
+
+
+# @app.route('/astronauts-with-aiohttp')
+# async def astronauts_with_aiohttp():
+#     async with aiohttp.ClientSession() as session:
+#         async with session.get(SPACE_API) as response:
+#             return f'Количество космонавтов в данный момент:'
+
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(astronauts_with_aiohttp())
