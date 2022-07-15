094c13cf029a107efacc053bbe32acfe67ac6ecb
00-hidan-00 <david.taranov@gmail.com>
2022-07-15 17:15:00 +0200

    :sponge: Cleaning project

--- app.py
+++ app.py
@@ -1,65 +1,62 @@
-import csv
-import io
+import sqlite3
 
-import requests
-from faker import Faker
 from flask import Flask
 from webargs import fields
 from webargs.flaskparser import use_args
 
-from constants import SPACE_API, CHARACTERISTICS_URL
-from settings import ROOT_PATH
+from settings import DB_PHONES_PATH
 
 app = Flask(__name__)
-faker = Faker()
 
 
-@app.route('/read-lesson')
-def read_lesson() -> str:
-    return ROOT_PATH.joinpath('lesson_4.py').read_text()
+class Connection:
+    def __init__(self):
+        self._connection: sqlite3.Connection | None = None
+
+    def __enter__(self):
+        self._connection = sqlite3.connect(DB_PHONES_PATH)
+        self._connection.row_factory = sqlite3.Row
+        return self._connection
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._connection.close()
 
 
-@app.route('/read-README')
-def read_README() -> str:
-    with open('README.md', 'r') as file:
-        data = file.readlines()
-    return str(data)
+@app.route('/phones/create/')
+@use_args({"contactName": fields.Str(required=True), "phoneValue": fields.Int(required=True)}, location="query")
+def phones__create(args):
+    with Connection() as connection:
+        with connection:
+            connection.execute(
+                'INSERT INTO phones (contactName, phoneValue) VALUES (:contactName, :phoneValue);',
+                {'contactName': args['contactName'], 'phoneValue': args['phoneValue']}
+            )
+    return 'OK'
 
 
-@app.route('/generate-users/')
-@use_args({'amount': fields.Int(missing=10)}, location="query")
-def generate_users(args: int) -> str:
-    users_list = [faker.unique.first_name() for _ in range(args['amount'])]
-    formatted_users_list = [f"""<p>{user} {str(user).lower()}@mail.co</p>""" for user in users_list]
-    new_users = ''.join(formatted_users_list)
-    return new_users
+@app.route('/phones/update/<int:phoneID>')
+@use_args({"phoneValue": fields.Int(required=True), "contactName": fields.Str(required=True)}, location="query")
+def phones__update(args, phoneID):
+    with Connection() as connection:
+        with connection:
+            connection.execute(
+                'UPDATE phones '
+                'SET phoneValue=:phoneValue, contactName=:contactName '
+                'WHERE phoneID=:phoneID;',
+                {'phoneValue': args['phoneValue'], 'contactName': args['contactName'], 'phoneID': phoneID}
+            )
+    return 'OK'
 
 
-@app.route('/astronauts-with-requests')
-def astronauts_with_requests() -> str:
-    response = requests.get(SPACE_API)
-    result = response.json()
-    people_list = ''.join([f"<li>{people['name']}</li>" for people in result["people"]])
-    return f"""<h1>Количество астронавтов: {result['number']}</h1>
-    <ul>Имена: {people_list}</ul>
-"""
-
-
-@app.route('/characteristics')
-def characteristics() -> str:
-    height_list = []
-    weight_list = []
-    response = requests.get(CHARACTERISTICS_URL)
-    csv_data = io.StringIO(response.text)
-    reader = csv.DictReader(csv_data, delimiter=',')
-    for row in reader:
-        height_list.append(float(row[' "Height(Inches)"']))
-        weight_list.append(float(row[' "Weight(Pounds)"']))
-    average_height = round(((max(height_list) + min(height_list)) / 2) * 2.54, 2)
-    average_weight = round(((max(weight_list) + min(weight_list)) / 2) / 2.2046, 2)
-    return f"""<p>Средний вес: {average_weight} кг</p>
-    <p>Средний рост: {average_height} см</p>    
-"""
+@app.route('/phones/delete/<int:phoneID>')
+def phones__delete(phoneID):
+    with Connection() as connection:
+        with connection:
+            connection.execute(
+                'DELETE FROM phones WHERE (phoneID=:phoneID);',
+                {'phoneID': phoneID}
+            )
+    return 'OK'
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -3,6 +3,4 @@ from typing import Final
 
 ROOT_PATH: Final[pathlib.Path] = pathlib.Path(__file__).parent
 
-DB_PATH: Final[pathlib.Path] = ROOT_PATH.joinpath('db', 'db.sqlite')
-
 DB_PHONES_PATH: Final[pathlib.Path] = ROOT_PATH.joinpath('db', 'db_phones.sqlite')
