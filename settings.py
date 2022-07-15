c8f7eaed5df80362b327a66f3ceca8bfa315d835
00-hidan-00 <david.taranov@gmail.com>
2022-07-15 17:15:31 +0200

    :sponge: Cleaning project

--- settings.py
+++ settings.py
@@ -2,7 +2,3 @@ import pathlib
 from typing import Final
 
 ROOT_PATH: Final[pathlib.Path] = pathlib.Path(__file__).parent
-
-DB_PATH: Final[pathlib.Path] = ROOT_PATH.joinpath('db', 'db.sqlite')
-
-DB_PHONES_PATH: Final[pathlib.Path] = ROOT_PATH.joinpath('db', 'db_phones.sqlite')
