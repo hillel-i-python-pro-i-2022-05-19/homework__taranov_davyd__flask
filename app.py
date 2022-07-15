d9fe889920d3145ceb83d353cdd7f8ec5f4b4936
00-hidan-00 <david.taranov@gmail.com>
2022-07-15 17:20:35 +0200

    :sponge: Cleaning project

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import csv
 import io
+import sqlite3
 
 import requests
 from faker import Faker
@@ -8,18 +9,23 @@ from webargs import fields
 from webargs.flaskparser import use_args
 
 from constants import SPACE_API, CHARACTERISTICS_URL
-from settings import ROOT_PATH
+from settings import ROOT_PATH, DB_PHONES_PATH
 
 app = Flask(__name__)
 faker = Faker()
 
 
-@app.route('/read-lesson')
-def read_lesson() -> str:
-    return ROOT_PATH.joinpath('lesson_4.py').read_text()
+@app.route('/')
+def hello_world():
+    return '<h1>Hello World!</h1>'
 
 
 @app.route('/read-README')
+def read_lesson() -> str:
+    return ROOT_PATH.joinpath('README.md').read_text()
+
+
+@app.route('/read-README-classical-method')
 def read_README() -> str:
     with open('README.md', 'r') as file:
         data = file.readlines()
@@ -60,6 +66,63 @@ def characteristics() -> str:
     return f"""<p>Средний вес: {average_weight} кг</p>
     <p>Средний рост: {average_height} см</p>    
 """
+
+
+class Connection:
+    def __init__(self):
+        self._connection: sqlite3.Connection | None = None
+
+    def __enter__(self):
+        self._connection = sqlite3.connect(DB_PHONES_PATH)
+        self._connection.row_factory = sqlite3.Row
+        return self._connection
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._connection.close()
+
+
+@app.route('/phones/create/')
+@use_args({"contactName": fields.Str(required=True), "phoneValue": fields.Int(required=True)}, location="query")
+def phones__create(args):
+    with Connection() as connection:
+        with connection:
+            connection.execute(
+                'INSERT INTO phones (contactName, phoneValue) VALUES (:contactName, :phoneValue);',
+                {'contactName': args['contactName'], 'phoneValue': args['phoneValue']}
+            )
+    return 'OK'
+
+
+@app.route('/phones/read/')
+def phones__read_all():
+    with Connection() as connection:
+        phones = connection.execute("SELECT * FROM phones;").fetchall()
+    return '<br>'.join([f'''{phone['phoneID']}: {phone['contactName']} - {phone['phoneValue']}''' for phone in phones])
+
+
+@app.route('/phones/update/<int:phoneID>')
+@use_args({"phoneValue": fields.Int(required=True), "contactName": fields.Str(required=True)}, location="query")
+def phones__update(args, phoneID):
+    with Connection() as connection:
+        with connection:
+            connection.execute(
+                'UPDATE phones '
+                'SET phoneValue=:phoneValue, contactName=:contactName '
+                'WHERE phoneID=:phoneID;',
+                {'phoneValue': args['phoneValue'], 'contactName': args['contactName'], 'phoneID': phoneID}
+            )
+    return 'OK'
+
+
+@app.route('/phones/delete/<int:phoneID>')
+def phones__delete(phoneID):
+    with Connection() as connection:
+        with connection:
+            connection.execute(
+                'DELETE FROM phones WHERE (phoneID=:phoneID);',
+                {'phoneID': phoneID}
+            )
+    return 'OK'
 
 
 if __name__ == '__main__':
